05f7b50d19fad09093fb538b50d64b70b11c831f
Salim Dason <dasonsalim@outlook.com>
2023-03-07 12:38:44 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/salimdason/wasabicli',
-    version='1.0.0',
+    version='3.0.2',
     zip_safe=False,
 )
